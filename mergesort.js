122659bac4724cb61f92ed20a68ec42fee09eb40
Dalton Spinas <daltonspinas@gmail.com>
2018-11-05 14:15:59 -0800

    working split function

--- mergesort.js
+++ mergesort.js
@@ -1,11 +1,18 @@
-function split(){
-
+function split(arr){
+let midPoint = Math.floor(arr.length / 2);
+let arr1= arr.slice(0,midPoint);
+let arr2= arr.slice(midPoint);
+return [arr1,arr2];
 }
 
-function merge(){
+function merge(arr){
 
 }
 
 function mergeSort(){
 
 }
+
+let testArr = [9,5,6,4,7,2,3];
+let testOne = [1];
+console.log(split(testOne));
